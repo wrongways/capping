4a8710a90f23034bc0931d7e9790a135ccf66047
Wrongways <jez@wain.name>
2023-04-25 13:11:48 +0000

    add simple_logger

--- src/bin/log_rapl.rs
+++ src/bin/log_rapl.rs
@@ -1,11 +1,14 @@
 use glob::glob;
 use log::{trace, error};
+use simple_logger;
 
 const RAPL_DIR: &str = "/sys/devices/virtual/powercap/intel-rapl/";
 const RAPL_CORE_GLOB: &str = "intel-rapl:?/intel-rapl:?:0/energy_uj";
 
 fn main() {
+   simple_logger::SimpleLogger::new().env().init().unwrap();
    let rapl_glob = format!("{RAPL_DIR}{RAPL_CORE_GLOB}");
+   trace!("{rapl_glob}");
    for rapl_file in glob(&rapl_glob).expect("Failed to read rapl glob") {
       match rapl_file {
           Ok(path) => trace!("{:?}", path.display()),

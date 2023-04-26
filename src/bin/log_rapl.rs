c60c6236ec3b934d3b3b7232e16a3495daf3719c
Wrongways <jez@wain.name>
2023-04-26 11:48:13 +0000

    Save energy readings to file

--- src/bin/log_rapl.rs
+++ src/bin/log_rapl.rs
@@ -1,27 +1,31 @@
 use glob::glob;
-use log::{trace, debug};
+use log::{trace, debug, error};
 use simple_logger;
-use std::fs;
-use std::path::PathBuf;
+use std::fs::{self, OpenOptions};
 use chrono::{self, DateTime, Local};
 use std::ffi::OsString;
 use std::thread::sleep;
 use std::time::Duration;
 use std::str::FromStr;
+use std::string::ToString;
+use std::borrow::Cow;
+use std::io::Write;
+use std::convert::AsRef;
 
 const RAPL_DIR: &str = "/sys/devices/virtual/powercap/intel-rapl/";
 const RAPL_CORE_GLOB: &str = "intel-rapl:?/intel-rapl:?:0/energy_uj";
+const STATS_FILE: &str = "rapl_stats.csv";
 
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
 struct RAPL_Data {
     pub timestamp: DateTime<Local>,
-    pub domain: OsString,
+    pub domain: String,
     pub energy: u64,
 }
 
 impl RAPL_Data {
-    fn new(domain: OsString, energy: u64) -> Self {
+    fn new(domain: String, energy: u64) -> Self {
         Self {
             timestamp: Local::now(),
             domain,
@@ -30,22 +34,38 @@ impl RAPL_Data {
     }
 }
 
-fn read_energy(filename: &OsString) -> u64 {
+fn read_energy(filename: &str) -> u64 {
     let energy_reading: String = fs::read_to_string(filename).unwrap();
-    trace!("read_energy({:?} => {}", filename, energy_reading);
+    trace!("read_energy string({:?} => {}", filename, energy_reading);
     let energy_reading: u64 = u64::from_str(energy_reading.trim()).unwrap();
-    trace!("read_energy({:?} => {}", filename, energy_reading);
+    trace!("read_energy u64({:?} => {}", filename, energy_reading);
     energy_reading
+}
+
+fn write_stats(stats: &Vec<RAPL_Data>) {
+    let mut outfile = OpenOptions::new()
+        .append(true)
+        .open(STATS_FILE)
+        .unwrap();
+
+    for stat in stats {
+        if let Err(why) = writeln!(&mut outfile, "{},{},{}", stat.timestamp, stat.domain, stat.energy) {
+            error!("Couldn't write to file {}: {}", STATS_FILE, why);
+        }
+    }
 }
 
 fn main() {
     simple_logger::SimpleLogger::new().env().init().unwrap();
     let rapl_glob = format!("{RAPL_DIR}{RAPL_CORE_GLOB}");
     let rapl_paths = glob(&rapl_glob).expect("Failed to read rapl glob");
-    let mut rapl_files = Vec::<OsString>::new();
+    let mut rapl_files = Vec::<String>::new();
     for p in rapl_paths {
         match p {
-            Ok(path) => rapl_files.push(path.into_os_string()),
+            Ok(path) => {
+                let path = path.into_os_string().into_string().unwrap();
+                rapl_files.push(path);
+            },
             Err(why) => debug!("Titsup on path: {why:?}"),
         }
     }
@@ -54,12 +74,11 @@ fn main() {
     for i in 0..5 {
         for rapl_file in &rapl_files {
             debug!("Pushing new energy reading: {i}");
-            energy_readings.push(RAPL_Data::new(rapl_file.clone(), read_energy(rapl_file)));
+            energy_readings.push(RAPL_Data::new(rapl_file.to_string(), read_energy(rapl_file)));
         }
         sleep(Duration::from_secs(1));
     }
 
-    for datapoint in energy_readings {
-        trace!("Datapoint: {:?}", &datapoint);
-    }
+    trace!("Saving stats");
+    write_stats(&energy_readings);
 }

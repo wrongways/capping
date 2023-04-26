7d8610e8253536e9f2adc3220dee54fc46ab06a5
Wrongways <jez@wain.name>
2023-04-26 12:24:03 +0000

    extract rapl domain from filepath

--- src/bin/log_rapl.rs
+++ src/bin/log_rapl.rs
@@ -1,13 +1,14 @@
 use glob::glob;
 use log::{trace, debug, error};
 use simple_logger;
-use std::fs::{self, OpenOptions};
 use chrono::{self, DateTime, Local};
 use std::thread::sleep;
 use std::time::Duration;
 use std::str::FromStr;
-use std::string::ToString;
 use std::io::Write;
+use std::path::Path;
+use std::fs::{self, OpenOptions};
+
 
 const RAPL_DIR: &str = "/sys/devices/virtual/powercap/intel-rapl/";
 const RAPL_CORE_GLOB: &str = "intel-rapl:?/intel-rapl:?:0/energy_uj";
@@ -53,6 +54,18 @@ fn write_stats(stats: &Vec<RAPL_Data>) {
     }
 }
 
+fn domain_from_path(path: &str) -> String {
+    trace!("domain_from_path({})", path);
+    let path = Path::new(path);
+    let parent_dir = path.parent().expect("failed to get parent").file_name().expect("couldn't get parent filename");
+    trace!("domain_from_path - parent dir: {:?}", parent_dir);
+    let rapl_device: String = parent_dir.to_os_string().into_string().expect("Couldn't create string");
+    let rapl_device_parts: Vec<&str> = rapl_device.split(':').collect();
+    let domain = format!("{}:{}", rapl_device_parts[1], rapl_device_parts[2]);
+    debug!("domain_from_path({}) -> {}", path.display(), domain);
+    return domain;
+}
+
 fn main() {
     simple_logger::SimpleLogger::new().env().init().unwrap();
     let rapl_glob = format!("{RAPL_DIR}{RAPL_CORE_GLOB}");
@@ -71,8 +84,9 @@ fn main() {
 
     for i in 0..5 {
         for rapl_file in &rapl_files {
+            let rapl_domain = domain_from_path(rapl_file);
             debug!("Pushing new energy reading: {i}");
-            energy_readings.push(RAPL_Data::new(rapl_file.to_string(), read_energy(rapl_file)));
+            energy_readings.push(RAPL_Data::new(rapl_domain, read_energy(rapl_file)));
         }
         sleep(Duration::from_secs(1));
     }

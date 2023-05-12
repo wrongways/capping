b276ae9257bdd4b2cd8c00f27aa5e9f919b86ad9
Wrongways <jez@wain.name>
2023-05-12 06:22:13 +0000

    use debug display for bmc to hide password in logs

--- src/bmc.rs
+++ src/bmc.rs
@@ -76,7 +76,7 @@ impl BMC {
     fn run_command(&self, bmc_command: &str) -> String {
         // Concatenate command with the credentials
         let ipmi_args = format!("{self} {bmc_command}");
-        trace!("BMC running command: {ipmi_args}");
+        trace!("BMC running command: {self:?} {bmc_command}");
         // process::Command requires arguments as an array
         let ipmi_args: Vec<&str> = ipmi_args.split_whitespace().collect();
 
